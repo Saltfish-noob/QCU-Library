112484999d27ab0939701d5b84a4275a3dd3756a
Saltfish-noob <52415262+Saltfish-noob@users.noreply.github.com>
2022-05-28 16:26:06 +0800

    Add files via upload

---
+++ account.sql
@@ -0,0 +1,13 @@
+
+
+create table account(
+	id int primary key auto_increment,
+	name varchar(40),
+	money float
+)character set utf8 collate utf8_general_ci;
+
+insert into account(name,money) values('aaa',1000);
+insert into account(name,money) values('bbb',1000);
+insert into account(name,money) values('ccc',1000);
+
+
